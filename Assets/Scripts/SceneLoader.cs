02a409e34865b07307b936e400a7d075dfe9ffbf
22ghoshi <ishaanghosh10@gmail.com>
2019-02-28 13:52:20 -0500

    fixed scene loaders

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneLoader : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public BoxCollider2D Box;
+    public string scene;
+
+    void Start()
+    {
+        GameObject Player = GameObject.Find("Player");
+        Playerbadges playerbadges = Player.GetComponent<PlayerBadges>;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void OnCollisionEnter2D(Collision2D col)
+    {
+        SceneManager.LoadScene(scene);
+    }
+}
