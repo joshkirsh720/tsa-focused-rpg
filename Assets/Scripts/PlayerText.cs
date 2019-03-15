3870698e4db23c85efdd09f369e377a2ff051313
22ghoshi <ishaanghosh10@gmail.com>
2019-03-15 12:10:11 -0400

    added minorly broken text system/waterfall

--- Assets/Scripts/PlayerText.cs
+++ Assets/Scripts/PlayerText.cs
@@ -19,13 +19,40 @@ public class PlayerText : MonoBehaviour
         Box.SetActive(true);
         if (typewriter)
         {
+            Tmptxt.ForceMeshUpdate();
+            int currentCharacter = 0;
             int textf = text.Length + 1;
-            for (int x = 0; x < textf; x++)
+            Tmptxt.text = text;
+            Tmptxt.color = new Color32(0, 0, 0, 255);
+            TMP_TextInfo textInfo = Tmptxt.textInfo;
+            Color32[] newVertexColors;
+            Color32 c0 = Tmptxt.color;
+            int characterCount = textInfo.characterCount;
+            while (currentCharacter < characterCount + 1)
+            {
+                
+                int materialIndex = textInfo.characterInfo[currentCharacter].materialReferenceIndex;
+                newVertexColors = textInfo.meshInfo[materialIndex].colors32;
+                int vertexIndex = textInfo.characterInfo[currentCharacter].vertexIndex;
+                c0 = new Color32(255, 255, 255, 255);
+
+                newVertexColors[vertexIndex + 0] = c0;
+                newVertexColors[vertexIndex + 1] = c0;
+                newVertexColors[vertexIndex + 2] = c0;
+                newVertexColors[vertexIndex + 3] = c0;
+                Tmptxt.UpdateVertexData(TMP_VertexDataUpdateFlags.Colors32);
+                currentCharacter = (currentCharacter + 1);
+
+                yield return new WaitForSeconds(0.05f);
+                Debug.Log(characterCount);
+            }
+            /*for (int x = 0; x < textf; x++)
             {
                 string display = text.Substring(0, x);
                 Tmptxt.text = display;
+                Tmptxt.color = new Color32(51, 51, 255, 255);
                 yield return new WaitForSeconds(.065f);
-            }
+            }*/
         }
         else
         {
